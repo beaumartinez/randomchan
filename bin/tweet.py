566025e8afd8bc17ee3142cc2be7c97bbb4fd383
Beau <beau@beaumartinez.com>
2013-02-28 20:30:22 +0000

    Bye bye OAuth keys
    We're public now

--- bin/tweet.py
+++ bin/tweet.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from sys import path
+from sys import argv, path
 
 import requests
 from requests.auth import OAuth1
@@ -10,14 +10,8 @@ path.insert(0, '.')
 from randomchan import random_formatted_tweetable_post
 
 
-def _tweet(content):
-    client_key = u'El04Ou8ZJPhBtmJeCf2KA'
-    client_secret = u'iiLjxG0cpf9TL7lyJEeuQOxm87fUWnMwzpXxjLEK8E'
-    resource_owner_key = u'968749950-251WsvFviXmcob2qg6LRkxybRjlBrlwuYEHga3Eq'
-    resource_owner_secret = u'Vzn0guHbJKiIPjaGvy3UOdebPnanVP79080rfWK2TI'
-
-    oauth = OAuth1(client_key, client_secret, resource_owner_key,
-                   resource_owner_secret, signature_type='auth_header')
+def _tweet(keys, content):
+    oauth = OAuth1(*keys, signature_type='auth_header')
 
     response = requests.post('https://api.twitter.com/1.1/statuses/'
                              'update.json', data={
@@ -28,7 +22,10 @@ def _tweet(content):
 
 
 if __name__ == '__main__':
+    keys = argv[1:5]
+    keys = tuple(unicode(key) for key in keys)
+
     post = random_formatted_tweetable_post('v')
     post = '{} #4chan #Shit4chanSays'.format(post)
 
-    _tweet(post)
+    _tweet(keys, post)
